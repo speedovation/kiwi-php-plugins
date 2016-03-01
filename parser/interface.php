6dfd3dd634cd85ac82ed00bf4f3e025c91a71bc9
Yash <yash@speedovation.com>
2016-03-01 22:24:43 +0530

    Refactor

--- parser/interface.php
+++ parser/interface.php
@@ -25,54 +25,6 @@ function return_result($method, $args)
 }
 
 
-/*
-Refactorings
-Extract Method   
-php refactor.phar extract-method <file_path> <line-range> <new-method>
-*/
-function extractMethod()
-{
-}
-
-
-/*
-Rename Local Variable
-php refactor.phar rename-local-variable <file_path> <line> <old-name> <new-name>
-*/
-function renameLocalVariable()
-{
-    
-}
-
-
-/*
-Convert Local to Instance Variable
-php refactor.phar convert-local-to-instance-variable <file_path> <line> <variable>
-*/
-function convertLocalToInstanceVariable()
-{
-    
-}
-
-
-/*
-Rename Class and Namespaces
-php refactor.phar fix-class-names <dir>
-*/
-function renameClassAndNamespaces()
-{
-    
-}
-
-
-/*
-Optimize use statements
-php refactor.phar optimize-use <file_path>
-*/
-function optimizeUse()
-{
-    
-}
 
 
 function parsePhp($json)
--- refactor/interface.php
+++ refactor/interface.php
@@ -1,11 +1,61 @@
 #!/usr/bin/env php
 <?php
 
-if ((!@include __DIR__ . '/../../autoload.php') && (!@include __DIR__ . '/../../vendor/autoload.php')) {
-    die("You must set up the project dependencies, run composer install");
-}
+
+include __DIR__ . '/../vendor/autoload.php';
 
 use QafooLabs\Refactoring\Adapters\Symfony\CliApplication;
 
 $application = new CliApplication();
 $application->run();
+
+
+/*
+Refactorings
+Extract Method   
+php refactor.phar extract-method <file_path> <line-range> <new-method>
+*/
+function extractMethod()
+{
+    $try = '';
+}
+
+
+/*
+Rename Local Variable
+php refactor.phar rename-local-variable <file_path> <line> <old-name> <new-name>
+*/
+function renameLocalVariable()
+{
+    
+}
+
+
+/*
+Convert Local to Instance Variable
+php refactor.phar convert-local-to-instance-variable <file_path> <line> <variable>
+*/
+function convertLocalToInstanceVariable()
+{
+    
+}
+
+
+/*
+Rename Class and Namespaces
+php refactor.phar fix-class-names <dir>
+*/
+function renameClassAndNamespaces()
+{
+    
+}
+
+
+/*
+Optimize use statements
+php refactor.phar optimize-use <file_path>
+*/
+function optimizeUse()
+{
+    
+}

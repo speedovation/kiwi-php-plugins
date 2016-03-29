0470ed2bce5ba2d9e6213469baabb735dd373790
Yash <yash@speedovation.com>
2016-03-29 18:32:18 +0530

    Syntax

--- src/Plugins/SyntaxChecker/Logic/SyntaxChecker.php
+++ src/Plugins/SyntaxChecker/Logic/SyntaxChecker.php
@@ -70,4 +70,59 @@ class SyntaxChecker
         
     }
     
+    function checkSyntax($json)
+    {
+        //bool runkit_lint ( string $code )
+        
+        //runkit_lint();
+        
+        $json = str_replace("'","",$json);
+        $json = str_replace("\\","",$json);
+        $v = json_decode($json);
+        
+        
+        echo "\n\n";
+        
+        $connection = \Tivoka\Client::connect(array('host' => '127.0.0.1', 'port' => 9040));
+        
+        $request = $connection->sendRequest('text',array('file_name'=> $v->file_path ));
+        
+        if(!$request->isError())
+        {
+            //$r = runkit_lint( $request->result );
+            $code = $request->result;
+            
+            $r = $this->checkSyntaxPhp($code,$v->file_path);
+            
+            
+            if(!empty($r) )
+            {
+                echo "Inside Error";
+                
+                //Call API to mark error spot on editor
+                $connection = \Tivoka\Client::connect(array('host' => '127.0.0.1', 'port' => 9040));
+                
+                $request = $connection->sendRequest('setMarkers',array('start' =>  $e->getStartLine(),
+                'end'  => $e->getEndLine(),
+                'file_name' => $filename
+                ));
+                
+                echo "Calling finised";
+            }
+            
+            echo "R: ".$r ."\n\n";
+        }
+        else
+        {
+            print 'Error '.$request->error.': '.$request->errorMessage;
+            var_dump($request->errorData);
+        }
+        
+        //echo "\n\nDone: Checking Syntax of PHP File\n\n";
+        
+        
+        
+    }
+    
+    
 }
